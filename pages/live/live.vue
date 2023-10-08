29669d85b36f42f4ec9c2a5586503d1ecc469255
cyw <cyw19931210@163.com>
2023-10-08 15:36:31 +0800

    test:live-player

---
+++ pages/live/live.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="live">
+		<live-player
+			id="player"
+			class="live__player"
+			src="rtmp://36.111.198.103:28936/live/652257e848bfa0a901edbd06"
+			mode="live"
+			autoplay
+			@statechange="handleStateChange"
+		/>
+	</view>
+</template>
+
+<script setup lang="ts">
+import { LivePlayerOnStatechangeEvent } from '@uni-helper/uni-app-types'
+
+const handleStateChange = (e: LivePlayerOnStatechangeEvent) => {
+	console.log('live-player code:', e.detail.code)
+}
+</script>
+
+<style scoped lang="scss">
+.live {
+	width: 100%;
+	height: 100vh;
+	&__player {
+		width: 100%;
+		height: 100%;
+	}
+}
+</style>
